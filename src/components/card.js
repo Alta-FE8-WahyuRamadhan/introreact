28c60cdab268b569fde80dd38b71601a17c4ead7
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-30 06:18:14 +0700

    tambah gambar

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 const Card = () => {
   return (
     <>
       <img
-        src="https://static.wikia.nocookie.net/naruto/images/b/bb/Itachi.png/revision/latest?cb=20151217131404&path-prefix=id"
-        alt="itachi"
+        src="https://p4.wallpaperbetter.com/wallpaper/780/49/86/saitama-one-punch-man-anime-gloves-wallpaper-preview.jpg"
+        alt="car"
+        width={150}
       />
 
       <p>Ini judul</p>
